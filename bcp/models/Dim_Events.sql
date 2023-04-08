7c1e38dc744c5e0165f39498b4d3c07f573e8ea3
DESKTOP-85J35KO\benmc <ben.mcchesney@gmail.com>
2023-04-08 09:38:38 -0700

    some initial minor changes

--- bcp/models/Dim_Events.sql
+++ bcp/models/Dim_Events.sql
@@ -7,6 +7,9 @@ select
 	, CONVERT( date , REPLACE( em.endDate  , ',' , '' ) ) as endDate 
 	, em.location
 	, em.ticketPrice
+	, et.totalPairings
+	, et.totalPlayers
+	, et.totalRounds
 FROM ( 
 select 
 	el.[index] as [Id]
@@ -14,7 +17,15 @@ select
 	, TRIM( REPLACE( url ,  'https://www.bestcoastpairings.com/event/' , '' ) ) as event_nk
 FROM events_links as el 
 ) as sub 
-LEFT OUTER JOIN [event_meta] as em 
+LEFT OUTER JOIN [event_meta] as em  
 ON sub.event_nk = em.eventId 
+LEFT OUTER JOIN ( 
+	select eventId , MAX( [round] ) as totalRounds
+	, count( distinct pairingId ) as totalPairings
+	, count( distinct playerListId ) as totalPlayers 
+	FROM pairingsData
+	GROUP BY eventId 
+) as et  
+ON et.eventId = sub.event_nk
 
 -- February, 11, 2023
